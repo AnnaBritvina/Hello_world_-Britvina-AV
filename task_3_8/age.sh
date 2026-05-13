f05e17755240f712a6acc24cfbd39a0e5fd80b4e
AnnaBritvina <britvinaanna54@gmail.com>
2026-05-13 15:21:02 +0300

    Delete task_3_8 directory

--- task_3_8/age.sh
+++
@@ -1,5 +0,0 @@
-CURRENT_YEAR=2026
-BIRTH_YEAR=2007
-AGE=$((CURRENT_YEAR - BIRTH_YEAR))
-echo "Текущий год: $CURRENT_YEAR"
-echo "Ваш примерный возраст: $AGE"
--- task_3_8/bmi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Введите массу тела (в кг):"
-read weight
-echo "Введите рост (в метрах):"
-read height
-bmi=$(echo "scale=2; $weight / ($height * $height)" | bc)
-echo "Ваш индекс массы тела (ИМТ): $bmi"
