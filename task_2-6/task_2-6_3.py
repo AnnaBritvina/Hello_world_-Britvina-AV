ea4e2d2032b11a76dbd6ed55119cf4c12a92f5ab
AnnaBritvina <britvinaanna54@gmail.com>
2026-05-13 15:18:43 +0300

    Delete task_2-6 directory

--- task_2-6/task_2-6_3.py
+++
@@ -1,8 +0,0 @@
-donor = input("Введите группу крови (0, A, B, AB) донора: ").strip().upper()
-recipient = input("Введите группу крови (0, A, B, AB) реципиента: ").strip().upper()
-if donor == recipient:
-    print("Переливание возможно")
-elif donor == "O":
-    print("Переливание возможно")
-else:
-    print("Переливание невозможно")
